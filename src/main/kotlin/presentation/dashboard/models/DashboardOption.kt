d8bc04ac205c87e2cec182870b1453d96baacbef
gustavo <gustavo.isaac@gmail.com>
2024-10-23 18:21:35 +0100

    DashboardOption

--- src/main/kotlin/presentation/dashboard/models/DashboardOption.kt
+++ src/main/kotlin/presentation/dashboard/models/DashboardOption.kt
@@ -1,9 +1,11 @@
 package presentation.dashboard.models
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ListAlt
 import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class DashboardOption(val icon: ImageVector, val description: String) {
     data object AddEvent: DashboardOption(icon = Icons.Filled.Bolt, description = "Adicionar evento")
+    data object EventList: DashboardOption(icon = Icons.AutoMirrored.Filled.ListAlt, description = "Listagem de eventos")
 }
