0fdcb71d6c90c747b53b4bd1f0537d7a73f92c58
gustavo <gustavo.isaac@gmail.com>
2024-10-27 16:03:24 +0000

    AddEventViewModel

--- src/main/kotlin/presentation/addevent/AddEventScreen.kt
+++ src/main/kotlin/presentation/addevent/AddEventScreen.kt
@@ -2,6 +2,7 @@
 
 package presentation.addevent
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
@@ -10,20 +11,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import core.ui.composables.topbar.PPTopBar
 import presentation.addevent.composables.EventDetails
+import presentation.addevent.composables.EventMenu
 import presentation.addevent.composables.HtmlViewer
 
 @Composable
 fun AddEventScreen(
-    onNavigateBack: () -> Unit
+    onNavigateBack: () -> Unit,
+    viewModel: AddEventViewModel = remember { AddEventViewModel() }
 ) {
     AddEventContent(
-        onNavigateBack = onNavigateBack
+        onNavigateBack = onNavigateBack,
+        state = viewModel.state.collectAsState().value,
+        events = viewModel::onTriggerEvent,
     )
 }
 
 @Composable
 private fun AddEventContent(
-    onNavigateBack: () -> Unit
+    onNavigateBack: () -> Unit,
+    state: AddEventState,
+    events: (AddEventEvent) -> Unit,
 ) {
     Scaffold(
         topBar = {
@@ -33,28 +40,47 @@ private fun AddEventContent(
             )
         },
         content = { innerPadding ->
-            Row(
+
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding),
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ){
-                HtmlViewer(
-                    url = "https://www.coolture.pt/event/feira-da-luz-2024-carnide-lisboa/",
-                    onGetHtmlCode = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth()
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                EventMenu(
+                    url = state.url,
+                    onUrlChange = { events.invoke(AddEventEvent.UpdateSearchQuery(it)) },
+                    onGetHtmlCode = { events.invoke(AddEventEvent.Search) },
+                    toggleHtmlCode = { events.invoke(AddEventEvent.ToggleHtmlCode) },
+                    modifier = Modifier.fillMaxWidth()
                 )
 
-                EventDetails(
-                    url = "https://www.coolture.pt/event/feira-da-luz-2024-carnide-lisboa/",
+                Row(
                     modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(),
-                    onGetHtmlCode = {
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ){
+                    EventDetails(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxWidth(),
+                        event = state.event
+                    )
+
+                    AnimatedVisibility(
+                        visible = state.showHtmlCode,
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                    ) {
+                        HtmlViewer(
+                            htmlContent = state.htmlContent,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
                     }
-                )
+                }
             }
         }
     )
@@ -64,7 +90,9 @@ private fun AddEventContent(
 @Composable
 private fun AddEventContentPreview() {
     AddEventContent(
-        onNavigateBack = {}
+        onNavigateBack = {},
+        state = AddEventState(),
+        events = {}
     )
 }
 
--- src/main/kotlin/presentation/addevent/composables/HtmlViewer.kt
+++ src/main/kotlin/presentation/addevent/composables/HtmlViewer.kt
@@ -22,83 +22,38 @@ import core.designsystem.Grey40
 import core.ui.composables.button.PPCircularIconButton
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import presentation.util.UrlUtil
 import java.io.IOException
 
 @Composable
 internal fun HtmlViewer(
-    url: String,
-    onGetHtmlCode: (String) -> Unit,
+    htmlContent: String,
     modifier: Modifier = Modifier,
 ) {
     val scrollState = rememberScrollState()
-    var htmlContent by remember { mutableStateOf("") }
 
-    LaunchedEffect(Unit) {
-        htmlContent = fetchHtmlWithOkHttp(url)
-    }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        htmlGetter(
-            url = url,
-            onGetHtmlCode = {}
-        )
-
-        Box(
-            modifier = modifier
-                .verticalScroll(scrollState)
-                .border(
-                    BorderStroke(1.dp, color = Grey40),
-                    shape = MaterialTheme.shapes.extraSmall
-                )
-                .background(color = Grey10)
-                .padding(16.dp)
-        ) {
-            BasicTextField(
-                value = htmlContent,
-                onValueChange = {  },
-                modifier = Modifier.fillMaxWidth(),
-                textStyle = TextStyle(fontSize = 16.sp),  // Style it like a Text component
-                readOnly = false  // Makes it non-editable
+    Box(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .border(
+                BorderStroke(1.dp, color = Grey40),
+                shape = MaterialTheme.shapes.extraSmall
             )
-        }
-    }
-}
-
-@Composable
-private fun htmlGetter(
-    url: String,
-    onGetHtmlCode: (String) -> Unit,
-){
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+            .background(color = Grey10)
+            .padding(16.dp)
     ) {
-        OutlinedTextField(
-            value = url,
-            label = { Text("Página web") },
-            onValueChange = {}
-        )
-
-        Button(
-            onClick = { onGetHtmlCode(url) },
-            content = {
-                Text("Obter html")
-            }
-        )
-        PPCircularIconButton(
-            onClick = {
-                UrlUtil.openUrl(url = url)
-            },
-            icon = Icons.Filled.FindInPage
+        BasicTextField(
+            value = htmlContent,
+            onValueChange = {  },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = TextStyle(fontSize = 16.sp),  // Style it like a Text component
+            readOnly = false  // Makes it non-editable
         )
     }
 }
+
 
 
 @Preview
@@ -106,26 +61,6 @@ private fun htmlGetter(
 private fun HtmlViewerPreview() {
     HtmlViewer(
         modifier = Modifier.fillMaxWidth(),
-        url = "",
-        onGetHtmlCode = {},
+        htmlContent = "",
     )
-}
-
-val client = OkHttpClient()
-
-fun cleanHtmlContent(html: String): String {
-    // Replace non-printable characters and trim extra whitespace
-    return html.replace(Regex("[\\n\\t\\r]"), "").trim()
-}
-
-fun formatHtmlContent(html: String): String {
-    // Replace unprintable characters with newlines
-    return html.replace(Regex("[\\u0000-\\u001F\\u007F-\\u009F]"), "\n").trim()
-}
-
-fun fetchHtmlWithOkHttp(url: String): String {
-    val request = Request.Builder().url(url).build()
-    client.newCall(request).execute().use { response ->
-        return if (!response.isSuccessful) throw IOException("Unexpected code $response") else response.body!!.string()
-    }
 }
